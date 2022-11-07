515b022c1d86cc6743185a5ea9dfc7c5308c88be
davindicode <Davindi09@gmail.com>
2022-11-07 13:29:16 +0000

    cleaned up model training

--- scripts/run_HDC.sh
+++ scripts/run_HDC.sh
@@ -4,7 +4,7 @@
 
 echo "Running block of 5 models..."
 
-python3 HDC.py --data_path /scratches/ramanujan_2/dl543/HDC_PartIII/ --mouse_id Mouse12 --session_id 120806 --phase wake --subset hdc --cv_folds 5 --cv 0 1 2 3 4 -1 --ncvx 2 --batch_size 10000 --max_epochs 2 --bin_size 160 --likelihood U-ec-3 --mapping svgp-64 --x_mode hd-omega-speed-x-y-time --lr 1e-2 --jitter 1e-5 --gpu 0 &
+python3 HDC.py --checkpoint_dir /scratches/ramanujan_2/dl543/HDC_PartIII/checkpoint/ --data_path /scratches/ramanujan_2/dl543/HDC_PartIII/ --mouse_id Mouse12 --session_id 120806 --phase wake --subset hdc --cv_folds 5 --cv 0 1 2 3 4 -1 --ncvx 2 --batch_size 10000 --max_epochs 2 --bin_size 160 --likelihood U-ec-3 --mapping svgp-64 --x_mode hd-omega-speed-x-y-time --lr 1e-2 --jitter 1e-5 --gpu 0 &
 BACK_PID1=$!
 
 python3 HDC.py --session_id 12-120806 --phase wake --cv_folds 5 --cv 0 1 2 3 4 -1 --ncvx 2 --batch_size 9000 --bin_size 30 --likelihood Uqd3 --mapping svgp64 --x_mode hd-w-s-pos-t --lr 1e-2 --jitter 1e-5 --gpu 0 &

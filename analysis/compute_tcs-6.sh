27333651c0ed59b5635b5c057f0c7b3600d94ce5
vnjaradi <vn283@cam.ac.uk>
2023-02-02 16:26:38 +0000

    cleanup

--- analysis/compute_tcs-6.sh
+++
@@ -1,9 +0,0 @@
-#!/bin/bash
-
-trap "kill 0" EXIT
- 
-#     'Mouse20': ['130515', '130516', '130517'],
-
-# Iterate the string array using for loop
-#python3 compute_tuning_curves.py --mouse_id Mouse28 --session_id 140310 --gpu 0
-python3 compute_tuning_curves.py --mouse_id Mouse17 --session_id 130203 --gpu 1
